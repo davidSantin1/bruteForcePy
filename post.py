d33c8a7598c0960fe0258f18678e75a325b97025
root <root@localhost.localdomain>
2015-11-20 11:01:12 -0300

    agregue un for

--- post.py
+++ post.py
@@ -2,16 +2,18 @@ import urllib
 import urllib2
 
 url = 'http://10.86.0.7/post.php'
+i=0
+for i in range(0, 3):
+	# Prepare the data
+	
+	values = {'msg' : '[python]' + str(i)} 
+	data = urllib.urlencode(values)
 
-# Prepare the data
-values = {'msg' : '[python]'}
-data = urllib.urlencode(values)
+	# Send HTTP POST request
+	req = urllib2.Request(url, data)
+	response = urllib2.urlopen(req)
 
-# Send HTTP POST request
-req = urllib2.Request(url, data)
-response = urllib2.urlopen(req)
-
-html = response.read()
+	html = response.read()
 
 # Print the result
-print html
+	print html
